dcb521175d6a9eabee71df53734e3d7ebc7db617
saqib867 <saqibhussain9910@gmail.com>
2025-06-15 22:35:02 +0500

    first commit

--- src/middleware/rate-limiter.ts
+++ src/middleware/rate-limiter.ts
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit";
 
 export const rateLimiter = rateLimit({
   windowMs: 5 * 60 * 1000,
-  max: 30,
+  max: 500,
   message: {
     success: false,
     message: "Too many requests. Please try again after 5 minutes.",
